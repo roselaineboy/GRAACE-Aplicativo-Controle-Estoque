a979aed779add99c4781599f941afff2851e8d07
Roselaine S. Boy <roselaineboy@yahoo.com.br>
2024-11-15 02:26:50 +0000

    feat: criação novas funções menu, produto e defproduto

--- classes/menu.py
+++ classes/menu.py
@@ -1,31 +1,75 @@
-import os
+from configurations.config import Settings
+from utils.bib import GlobalFunctions
+from classes.produto import Produto
 
-class Menu:
-    def __init__(self, opcao = none):
+class Menu(Settings):
+    def __init__(self, opcao = None):
         self.__opcao = opcao
+        self.GF = GlobalFunctions()
 
-#Inicio - Cria a função Menu   
+    def to_add_product(self):
+        msgret = ''
+        cadproduto = Produto()
+        msgret = cadproduto.receber_dados_novo_produto()
+        del(cadproduto)
+        return msgret
+
+    #Inicio - Cria a função Menu   
     def show_menu(self):
-        print(50 * '═')
-        print('\nBem-vindo ao ACE - Aplicativo de Controle de Estoque')
-        print(50 * '═')
+        len_row = 55
+        self.GF.limpar_tela()
+        print(len_row * '═')
+        print('Bem-vindo ao ACE - Aplicativo de Controle de Estoque')
+        print(len_row * '═')
         print('1 - Adicionar Produto')
         print('2 - Visualizar Estoque')
         print('3 - Alterar estoque')
         print('4 - Remover Estoque')
         print('5 - Log do Estoque')
         print('0 - Sair')
-        print(50 * '═')
+        print(len_row * '═')
+    #Inicio - Cria a função de selecionar as opções
 
-#Inicio - Cria a função de selecionar as opções
-        def choose_option(self):
-        self.__choose_option = input('\nEscolha uma das opções: ')
-        return self.__choose_option
+    def choose_option(self):
+        self.show_menu()
+
+        option = ''
+        msg_text = ''
+
+        while option != '0':
+            self.show_menu()
+            if msg_text != '':
+                print(f'{msg_text}')
+                msg_text = ""
+
+            option = input('\nEscolha uma das opções: ')
         
-        if option not in ['0', '1', '2', '3', '4', '5']:
-            print('\nOpção inválida!')
-        return option
+            if option not in ['0', '1', '2', '3', '4', '5']:
+                msg_text = 'Opção inválida!'
+            else:
+                if option == '1':
+                    #ace.to_add()
+                    msg_text = self.to_add_product()
+                    msg_text = 'Msg ao Adicionar Produto: ' + msg_text
+                elif option == '2':
+                    #ace.to_view()
+                    msg_text = 'Visualizar Estoque'
+                elif option == '3':
+                    #ace.to_go_out()
+                    msg_text = 'Alterar estoque'
+                elif option == '4':
+                    #ace.to_go_out()
+                    msg_text = 'Remover Estoque'
+                elif option == '5':
+                    #ace.to_go_out()
+                    msg_text = 'Log do Estoque'
+                else:
+                    msg_text = msg_text + '\npressione enter para exibir o menu novamente'
 
-#Cria a funcão para limpar a tela
-    def limpar_tela(self):
-        os.system('cls') if os.name == 'nt' else 'clear'
+        # fim while option
+    
+        self.GF.limpar_tela()
+        print('\nObrigado volte sempre!')
+
+menu = Menu()
+menu.choose_option()
---
+++ classes/produto.py
@@ -0,0 +1,43 @@
+from entities.def_produto import DefProduto
+from utils.bib import GlobalFunctions
+
+class Produto():
+    def __init__(self):
+        self.__produto = DefProduto()
+        self.__gf = GlobalFunctions()
+    
+    def view_produto(self):
+        self.__produto.view()
+
+    def receber_dados_novo_produto(self):
+        msg_validacao = 'Informe os dados do Produtos'
+
+        while msg_validacao != '':
+            self.__gf.limpar_tela()
+
+            if len(msg_validacao) > 0:
+                print(msg_validacao)
+                msg_validacao == ''
+            print('Ps.: para desistir digite -1 no código do produto e deixe os demais campos em branco')
+            self.__produto.codigo = input('Código: ')
+            if self.__produto.codigo != '-1':
+                self.__produto.nome = input('Nome: ')
+                self.__produto.categoria = input('Categoria: ')
+                self.__produto.localarmazenamento = input('Local de Armazenamento: ')
+                self.__produto.valorunitario = input('Valor Unitário: ')
+            
+            if self.__produto.codigo == '-1':
+                msg_validacao = ''
+            else:
+                msg_validacao = self.__produto.validar_conteudo()
+
+        # fim - while
+        self.__gf.limpar_tela()
+
+        if msg_validacao == "":
+            self.view_produto()
+            input('<ENTER>')
+            self.__gf.limpar_tela()
+
+        return "Produto cadastrado"
+
