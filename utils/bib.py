8d92d333adcf14b0b06fa69adba8871265117316
Roselaine S. Boy <roselaineboy@yahoo.com.br>
2024-11-20 23:05:52 +0000

    feat - novas funções adicionadas

--- utils/bib.py
+++ utils/bib.py
@@ -6,15 +6,90 @@
 #Docente: Adriano V. S. da Silva
 
 import os
+from IPython.display import clear_output
+import json
+import pandas as pd
+
 
 class Funcao_Global():
     
     def limpar_tela(self):
         """Limpa a tela do terminal, dependendo do sistema operacional."""
+
         sistema = os.name  # 'posix' para Unix-based, 'nt' para Windows
         if sistema == 'posix':  # Unix-based (Linux/macOS)
-            os.system('clear')
+            eh_colab = False
+            try:
+                import google.colab
+                eh_colab = True
+            except ImportError:
+                eh_colab = False
+
+            if eh_colab:
+                clear_output()
+            else:
+                os.system('clear')
         elif sistema == 'nt':  # Windows
             os.system('cls')
     # Fim - limpar_tela
-    
+
+    def verifica_arquivo(self, caminho):
+        mensagem_retorno = ''
+        # Verifica se o diretório existe, caso contrário, cria
+        diretorio = os.path.dirname(caminho)
+        diretorio_ok = False
+
+        if not os.path.exists(diretorio):
+            try:
+                os.makedirs(diretorio)
+                diretorio_ok = True
+            except Exception as e:
+                mensagem_retorno = f'Erro ao criar o diretorio: {e}'
+                # a fazer: adicionar mensagem no log
+        else:
+            diretorio_ok = True
+
+        # Verifica se o arquivo existe
+        if diretorio_ok:
+            if not os.path.exists(caminho):
+                # Cria o arquivo JSON vazio caso ele não exista
+                try:
+                    with open(caminho, 'w') as arquivo:
+                        lista_vazia = pd.DataFrame()
+                        lista_vazia.to_json(caminho, orient='records', indent=4)
+
+                except Exception as e:
+                    mensagem_retorno = mensagem_retorno + ' ' + f'Erro ao criar o arquivo: {e}'
+
+        return mensagem_retorno
+    # fim - verifica_arquivo
+
+    def eh_numero(self, s):
+        if (s.startswith('-') or s.startswith('+') ):
+            s = s[1:]  # Remove o sinal para verificar o restante
+        s = s.replace('.', '')  # Remove o ponto decimal, pois estamos no Brasil
+        s = s.replace(',', '.') # Trocando a vírgula pelo ponto decima, pois estamos no Brasil, mas o python não sabe disso
+
+        if '.' in s:
+            partes = s.split('.')
+            if len(partes) != 2 or not partes[0].isdigit() or not partes[1].isdigit():
+                return False
+        else:
+            if not s.isdigit():
+                return False
+        return True
+    # fim eh_numero
+
+    def transforma_em_float(self, s):
+        s = s.replace('.', '')  # Remove o ponto decimal, pois estamos no Brasil
+        s = s.replace(',', '.') # Trocando a vírgula pelo ponto decima, pois estamos no Brasil, mas o python não sabe disso
+
+        return float(s)
+    # fim transforma_em_float
+
+    def transforma_em_int(self, s):
+        s = s.replace('.', '')  # Remove o ponto decimal, pois estamos no Brasil
+        s = s.replace(',', '.') # Trocando a vírgula pelo ponto decima, pois estamos no Brasil, mas o python não sabe disso
+
+        return int(s)
+    # fim transforma_em_int
